0bfa52e81a087d0c385effb7118be7d71ab8b950
Ilyas Maazouz <ilyassmaazouz02@gmail.com>
2025-11-26 22:57:38 +0200

    Data Explainability phase

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\hp\Downloads\MLFLOW_HumanActivity\.venv
+set VIRTUAL_ENV=D:\Projet_ML\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
